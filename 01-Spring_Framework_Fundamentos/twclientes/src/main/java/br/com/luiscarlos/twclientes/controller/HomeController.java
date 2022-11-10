4f05600e053abd52220b92c7a1446be85422ad50
Luis Carlos <luiscarlosnors@gmail.com>
2022-11-10 20:52:55 -0300

    Passando informações do Controller para a View a Classe ModelAndView.

--- 01-Spring_Framework_Fundamentos/twclientes/src/main/java/br/com/luiscarlos/twclientes/controller/HomeController.java
+++ 01-Spring_Framework_Fundamentos/twclientes/src/main/java/br/com/luiscarlos/twclientes/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -24,5 +25,14 @@ public class HomeController {
     public String mensagemMap(ModelMap modelMap) {
         modelMap.addAttribute("mensagem", "Mensagem vinda do servido, ex Map");
         return "mensagem";
+    }
+
+    @GetMapping("/modelandview")
+    public ModelAndView mensagemModelAndView() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("mensagem");
+        modelAndView.addObject("mensagem", "Mensagem tipo Model e View");
+
+        return modelAndView;
     }
 }
