244d38d11c356c160cccea38ae99a67d0538f2cb
Luis Carlos <luiscarlosnors@gmail.com>
2022-11-10 20:45:59 -0300

    Passando informações do Controller para a View a Interface Model.

--- 01-Spring_Framework_Fundamentos/twclientes/src/main/java/br/com/luiscarlos/twclientes/controller/HomeController.java
+++ 01-Spring_Framework_Fundamentos/twclientes/src/main/java/br/com/luiscarlos/twclientes/controller/HomeController.java
@@ -1,6 +1,8 @@
 package br.com.luiscarlos.twclientes.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,5 +12,17 @@ public class HomeController {
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    @GetMapping("/mensagem")
+    public String mensagem(Model model) {
+        model.addAttribute("mensagem", "Mensagem vinda do servido");
+        return "mensagem";
+    }
+
+    @GetMapping("/map")
+    public String mensagemMap(ModelMap modelMap) {
+        modelMap.addAttribute("mensagem", "Mensagem vinda do servido, ex Map");
+        return "mensagem";
     }
 }
