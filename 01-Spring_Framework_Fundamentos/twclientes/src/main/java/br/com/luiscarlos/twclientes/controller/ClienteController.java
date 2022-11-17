730f843ed118954220fafc7dcc3638c4aa4afcd6
Luis Carlos <luiscarlosnors@gmail.com>
2022-11-16 21:08:23 -0300

    11.1 - Criando a action para exibir os detalhes de um Cliente.

--- 01-Spring_Framework_Fundamentos/twclientes/src/main/java/br/com/luiscarlos/twclientes/controller/ClienteController.java
+++ 01-Spring_Framework_Fundamentos/twclientes/src/main/java/br/com/luiscarlos/twclientes/controller/ClienteController.java
@@ -4,8 +4,8 @@ import br.com.luiscarlos.twclientes.model.Cliente;
 import br.com.luiscarlos.twclientes.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -23,6 +23,16 @@ public class ClienteController {
 
         List<Cliente> clienteList = clienteRepository.findAll();
         modelAndView.addObject("clientes", clienteList);
+
+        return modelAndView;
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView detalhar(@PathVariable Long id) {
+        ModelAndView modelAndView = new ModelAndView("cliente/detalhar.html");
+
+        Cliente cliente = clienteRepository.getReferenceById(id);
+        modelAndView.addObject("cliente", cliente);
 
         return modelAndView;
     }
